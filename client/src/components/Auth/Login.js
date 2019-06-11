a95a8c6faa9010ad247cea5478df48764eabae14
CrisRod <crodriguez85@gmail.com>
2019-06-11 16:47:10 -0400

    tokenId and isSignedIn true

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -6,15 +6,16 @@ import { withStyles } from "@material-ui/core/styles";
 
 const Login = ({ classes }) => {
   const responseGoogle = response => {
-    console.log(response)
+    // console.log(response)
+    const idToken = response.getAuthResponse().id_token
+    console.log({ idToken})
   }
-
-
   return(
     <GoogleLogin 
       clientId="226743835108-vv9vnn1r8t9aadvj6jrrmbropv510ggq.apps.googleusercontent.com"
       onSuccess={responseGoogle}
       onFailure={responseGoogle}
+      isSignedIn={true}
       />
   )
 };
