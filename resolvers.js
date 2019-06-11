fdd7fd2428749a048ede845c47b70f32e5636eee
CrisRod <crodriguez85@gmail.com>
2019-06-11 17:30:08 -0400

    Resolver using authentication on context

--- resolvers.js
+++ resolvers.js
@@ -1,12 +1,21 @@
+const { AuthenticationError } = require('apollo-server');
+
 const user = {
     _id: "1",
     name: "Cristian",
     email: "crodriguez85@gmail",
     picture: "http://www.cloudinary.com"
+};
+
+const authenticated = next => (root, args, ctx, info) => {
+    if(!ctx.currentUser) {
+        throw new AuthenticationError('You must be Logged in')
+    }
+    return next(root, args, ctx, info)
 }
 
 module.exports = {
     Query: {
-        me: () => user
+        me: authenticated((root, args, ctx) => AudioContext.currentUser)
     }
 }
