a1056fa1a4c25f223bd3f7d33e3d5ab5cfb0b58d
CrisRod <crodriguez85@gmail.com>
2019-06-11 16:24:24 -0400

    Google OAuth

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { GoogleLogin } from 'react-google-login';
 import { withStyles } from "@material-ui/core/styles";
+
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const responseGoogle = response => {
+    console.log(response)
+  }
+
+
+  return(
+    <GoogleLogin 
+      clientId="226743835108-vv9vnn1r8t9aadvj6jrrmbropv510ggq.apps.googleusercontent.com"
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      />
+  )
 };
 
 const styles = {
