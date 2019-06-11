dbef2cec104b3df6e5fb37babdd56022fc4172d2
CrisRod <crodriguez85@gmail.com>
2019-06-11 17:02:06 -0400

    Me_Query and GraphQLClient

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,15 +1,31 @@
 import React from "react";
+import { GraphQLClient } from 'graphql-request';
 import { GoogleLogin } from 'react-google-login';
 import { withStyles } from "@material-ui/core/styles";
 
 // import Typography from "@material-ui/core/Typography";
 
-const Login = ({ classes }) => {
-  const responseGoogle = response => {
-    // console.log(response)
-    const idToken = response.getAuthResponse().id_token
-    console.log({ idToken})
+const ME_QUERY = `
+{
+  me{
+    _id
+    name
+    email
+    picture
   }
+}` 
+
+const Login = ({ classes }) => {
+  const responseGoogle = async response => {
+    // console.log(response)
+    const idToken = response.getAuthResponse().id_token;
+    // console.log({ idToken})
+    const client = new GraphQLClient('http://localhost:4000/graphql', {
+      headers: { authorization: idToken }
+    })
+    const data = await client.request(ME_QUERY)
+    console.log({ data });
+  };
   return(
     <GoogleLogin 
       clientId="226743835108-vv9vnn1r8t9aadvj6jrrmbropv510ggq.apps.googleusercontent.com"
