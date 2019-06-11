6961d3da046a3b94f3993cfebca0d13ffa8ebb0a
CrisRod <crodriguez85@gmail.com>
2019-06-11 12:02:51 -0400

    Mongo Connected

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
 
 // Import Resolvers typeDefs
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
+
+// dotenv
+require('dotenv').config()
+
+// Mongoose connect
+mongoose.connect(process.env.MONGO_URI, { 
+    useNewUrlParser: true })
+    .then(() => console.log('DB Connected'))
+    .catch(err => console.error(err));
 
 const server = new ApolloServer({
     typeDefs,
