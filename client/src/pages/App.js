2359b152d1bd8262c3b2d429b5312407882c8736
CrisRod <crodriguez85@gmail.com>
2019-06-13 13:17:43 -0400

    Importing Map component

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import withRoot from '../withRoot';
 import Header from '../components/Header';
+import Map from '../components/Map';
 
 const App = () => {
     return (
-        <Header/>
+        <Fragment>
+            <Header/>
+            <Map/>
+        </Fragment>
     )
 }
 
