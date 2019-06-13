60683fcb801e5215f9105d2838ae38e893a92ae3
CrisRod <crodriguez85@gmail.com>
2019-06-13 14:14:40 -0400

    viewport with const

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,31 @@
 import React from "react";
+import ReactMapGL from 'react-map-gl';
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const viewport = {
+  latitude: -33.4087838,
+  longitude: -70.567070,
+  zoom: 13
+}
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoiY3JvZHJpZ3Vlejg1IiwiYSI6ImNqd3V2eGpzYTBsZmUzeW81NTJwemJicmQifQ.XoQ9-TPZNzQO6boBQ-3VXA"
+        { ...viewport }
+      >
+      
+
+      </ReactMapGL>
+    </div>
+  );
 };
 
 const styles = {
