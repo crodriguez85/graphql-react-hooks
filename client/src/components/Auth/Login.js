3cf3247864a8f41774de44016e4bbf6eb2048598
CrisRod <crodriguez85@gmail.com>
2019-06-13 09:56:21 -0400

    Try Catch Login

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -18,19 +18,22 @@ const ME_QUERY = `
 }` 
 
 const Login = ({ classes }) => {
-  const { dispatch } = useContext((Context))
+  const { dispatch } = useContext(Context);
 
-  const responseGoogle = async response => {
-    // console.log(response)
-    const idToken = response.getAuthResponse().id_token;
-    // console.log({ idToken})
-    const client = new GraphQLClient('http://localhost:4000/graphql', {
-      headers: { authorization: idToken }
-    })
-    const { me } = await client.request(ME_QUERY)
-    // console.log({ data });
-    dispatch({ type: "LOGIN_USER", payload: me})
+  const onSuccess = async googleUser => {
+    try {
+      const idToken = googleUser.getAuthResponse().id_token;
+      const client = new GraphQLClient('http://localhost:4000/graphql', {
+        headers: { authorization: idToken }
+      });
+      const { me } = await client.request(ME_QUERY);
+      // console.log({data})
+      dispatch({ type: "LOGIN_USER", payload: me });
+    } catch (err) {
+      onFailure(err);
+    }
   };
+
 
   const onFailure = err => {
     console.error("Error Loging In", err)
@@ -38,7 +41,7 @@ const Login = ({ classes }) => {
   return(
     <GoogleLogin 
       clientId="226743835108-vv9vnn1r8t9aadvj6jrrmbropv510ggq.apps.googleusercontent.com"
-      onSuccess={responseGoogle}
+      onSuccess={onSuccess}
       onFailure={onFailure}
       isSignedIn={true}
       />
