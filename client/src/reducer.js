4474b90d840f70119f08df80eb649a3845bbf55a
CrisRod <crodriguez85@gmail.com>
2019-06-13 17:14:37 -0400

    DELETE_DRAFT reducer

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -5,17 +5,20 @@ export default function reducer(state, { type, payload }) {
           ...state,
           currentUser: payload
         };
+
       case "IS_LOGGED_IN":
         return {
           ...state,
           isAuth: payload
         };
+
       case "SIGNOUT_USER":
         return {
           ...state,
           isAuth: false,
           currentUser: null
         };
+
       case "CREATE_DRAFT":
         return {
           ...state,
@@ -25,13 +28,20 @@ export default function reducer(state, { type, payload }) {
             longitude: 0
           }
         };
+
       case "UPDATE_DRAFT_LOCATION":
         return {
           ...state,
           draft: payload
         };
 
-            default:
+      case "DELETE_DRAFT":
+        return {
+          ...state,
+          draft: null
+        };
+
+        default:
                 return state;
     }
 }
